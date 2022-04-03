fdf49f7e465361b9cff66408a117f9bbf79047b2
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-03 15:39:40 +0300

    Update random.js

--- js/random.js
+++ js/random.js
@@ -14,5 +14,5 @@ function getRandomNumber(lower, upper) {
 // Call the function and pass it different values
 
 console.log( getRandomNumber(1,6))
-console.log( getRandomNumber(10,100))
+console.log( `${getRandomNumber(10,100)} is arandom number between 10 and 100.`)
 console.log( getRandomNumber(200,500))
